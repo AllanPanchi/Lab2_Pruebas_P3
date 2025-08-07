ba9d3ecdc1e7b99ef1a0bbf44ab6550283b666a1
unknown <avpanchi@espe.edu.ec>
2025-08-07 12:26:03 -0500

    Cambios V3

--- eslint.config.js
+++ eslint.config.js
@@ -6,8 +6,8 @@ export default [
             sourceType: 'module'
         },
         rules: {
-            "semi": ["error", "always"],
-            "quotes": ["error", "single"]
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single']
         }
     }
-]
+];
