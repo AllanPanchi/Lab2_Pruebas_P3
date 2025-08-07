2f11125edb67819713d9c01078d8cbf32e1150f0
unknown <avpanchi@espe.edu.ec>
2025-08-07 12:28:50 -0500

    Cambios V5

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,8 @@
 const { default: test } = require('node:test');
 const add = require('./sum').default;
 
-test('Suma 1 + 2 = 3', () => {
-    expect.equal(add(1, 2), 3);
+describe('Suma', () => {
+    test('Suma 1 + 2 = 3', () => {
+        expect.equal(add(1, 2), 3);
+    });
 });
