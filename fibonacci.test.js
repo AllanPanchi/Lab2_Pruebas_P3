7903f750b4c2e66077ebb845f0d3ac7859571984
unknown <avpanchi@espe.edu.ec>
2025-08-10 13:39:07 -0500

    Pruebas con error en fibonacci

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -3,6 +3,6 @@ const fib = require('./math.js');
 test(
     'Expect 13 when calculating the 7th Fibonacci number',
     () => {
-        expect(fib.fibonacci(7)).toBe(13);
+        expect(fib.fibonacci(0)).toBe(13);
     }
 );
